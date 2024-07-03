e814e73c75061713bf582a72c50f5863fa7ac618
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-07-03 02:52:17 +0000

    change

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,15 @@
 export default function updateUniqueItems(map) {
-	if (!(map instanceof Map)) {
-		throw new Error('Cannot process');
-	}
+  // check if map is an instance of Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-	map.forEach((quantity, item) => {
-		if (quantity === 1) {
-			map.set(item, 100);
-		}
-	});
+  // iterate over each item in the map
+  map.forEach((quantity, item) => {
+    // check if the quantity of the item is 1
+    if (quantity === 1) {
+      // update the quantity of the item to 100
+      map.set(item, 100);
+    }
+  });
 }
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,14 @@
 export default function groceriesList() {
-	const groceriesMap = new Map();
+  // Create a new Map to store the groceries
+  const groceriesMap = new Map();
 
-	groceriesMap.set('Apple', 10);
-	groceriesMap.set('Tomatoes', 10);
-	groceriesMap.set('Pasta', 1);
-	groceriesMap.set('Rice', 1);
-	groceriesMap.set('Banana', 5);
+  // Add groceries to the map with their quantities
+  groceriesMap.set('Apple', 10); // 10 Apples
+  groceriesMap.set('Tomatoes', 10); // 10 Tomatoes
+  groceriesMap.set('Pasta', 1); // 1 Pasta
+  groceriesMap.set('Rice', 1); // 1 Rice
+  groceriesMap.set('Banana', 5); // 5 Bananas
 
-	return groceriesMap;
+  // Return the groceries map
+  return groceriesMap;
 }
